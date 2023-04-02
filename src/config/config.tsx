48c4df077d270fb1713ce2f405798462372fa2ab
mgzparanoid <31665193+mgzparanoid@users.noreply.github.com>
2023-04-03 00:30:11 +0200

    Added a basic configuration file.

---
+++ src/config/config.tsx
@@ -0,0 +1,9 @@
+export interface Config{
+    authenticationToken: string;
+    initialCountry: string;
+}
+
+export const config: Config ={
+    authenticationToken: 'f51c75debe104f8899d14bb5734621a0',
+    initialCountry: 'pl'
+}
