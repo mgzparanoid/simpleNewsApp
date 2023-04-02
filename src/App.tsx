b63a590e548f85a8866983fe896bde3a6fd72e10
mgzparanoid <31665193+mgzparanoid@users.noreply.github.com>
2023-04-03 00:27:49 +0200

    Changed teh way the application is displayed.

--- src/App.tsx
+++ src/App.tsx
@@ -7,15 +7,22 @@ import Header from "./components/Header";
 import { BrowserRouter } from "react-router-dom";
 import { Provider } from "react-redux";
 import { store } from "./store/store";
+import { Grid } from "@mui/material";
 
 function App() {
   return (
     <Provider store={store}>
       <BrowserRouter>
-        <Header />
-        <Content />
-        <SideMenu />
-        <Footer />
+          <Header />
+          <Grid container display="flex">
+            <Grid item xs={2} sm={2} md={2}>
+              <SideMenu />
+            </Grid>
+            <Grid item xs={10} sm={10} md={10}>
+              <Content />
+            </Grid>
+          </Grid>
+          <Footer />
       </BrowserRouter>
     </Provider>
   );
