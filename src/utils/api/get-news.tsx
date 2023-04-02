94e37627e1a8305ba09ba3d3eb8e5f5045f7618a
mgzparanoid <31665193+mgzparanoid@users.noreply.github.com>
2023-04-03 00:31:26 +0200

    Added types and changed the api call handling.

--- src/utils/api/get-news.tsx
+++ src/utils/api/get-news.tsx
@@ -1,14 +1,43 @@
-import axios from "axios"
+import axios from "axios";
+import { config } from "../../config/config";
 
-type GetNewsResponse = { data?: any}
+export type GetNewsResponseSuccess = {
+  success: boolean;
+  totalResults: number;
+  data: Article[];
+};
+export type GetNewsResponseError = {
+  success: boolean;
+  message?: string;
+};
+
+export type Article = {
+  author: string;
+  content: string | null;
+  description: string | null;
+  publishedAt: string;
+  source: Source;
+  title: string;
+  url: string;
+  urlToImage: string | null;
+};
+
+export type Source = {
+  id: string;
+  name: string;
+};
 
 export interface GetNews {
-    (): Promise<GetNewsResponse>
-    // (): Promise<GetNewsResponse | ErrorResp>
+  (countryCode: string): Promise<GetNewsResponseSuccess | GetNewsResponseError>;
 }
 
-export const getNews: GetNews = async () => {
-    return axios('')
-    .then(resp => ({data: resp.data}))
-    // .catch(err => "Error while fetching projects")
-}
+export const getNews: GetNews = async (countryCode: string) => {
+  try {
+    const response = await axios(
+      `https://newsapi.org/v2/top-headlines?country=${countryCode}&apiKey=${config.authenticationToken}`
+    );
+    return { data: response.data.articles, success: true };
+  } catch {
+    return { message: "Couldn't fetch the news", success: false };
+  }
+};
