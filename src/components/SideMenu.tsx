b609408ea1ae29f0ed0c357d622aefb7757dafc1
mgzparanoid <31665193+mgzparanoid@users.noreply.github.com>
2023-04-03 00:29:58 +0200

    Added a very basic side menu that includes a short list of countries and their respecitve news.

--- src/components/SideMenu.tsx
+++ src/components/SideMenu.tsx
@@ -1,8 +1,29 @@
-import React from "react"
+import React from "react";
+import { Link } from "react-router-dom";
 
 const SideMenu: React.FC = () => {
-return(
-    <div>OlaBoga</div>
-)
-}
-export default SideMenu
+  return (
+    <>
+      <Link to="/country/pl">
+        <span style={{ textDecoration: "none" }}>Poland</span>
+      </Link>
+      <br />
+      <Link to="/country/gb">
+        <span style={{ textDecoration: "none" }}>Great Britain</span>
+      </Link>
+      <br />
+      <Link to="/country/jp">
+        <span style={{ textDecoration: "none" }}>Japan</span>
+      </Link>
+      <br />
+      <Link to="/country/de">
+        <span style={{ textDecoration: "none" }}>Germany</span>
+      </Link>{" "}
+      <br />
+      <Link to="/country/us">
+        <span style={{ textDecoration: "none" }}>USA</span>
+      </Link>
+    </>
+  );
+};
+export default SideMenu;
